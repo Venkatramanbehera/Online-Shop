607b633aa3d85ce7e1491a9d3046fd7a0036f675
Venkatramanbehera <Venkatramanbehera1@gmail.com>
2020-12-14 16:01:53 +0530

    dynamic url id passing

--- models/product.js
+++ models/product.js
@@ -21,10 +21,12 @@ module.exports = class Product {
   constructor(title, imageUrl , description , price) {
     this.title = title;
     this.imageUrl = imageUrl;
+    this.description = description;
     this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err =>{
@@ -35,5 +37,11 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductFromFile(cb);
+  }
+  static findById(id, cb){
+    getProductFromFile(products =>{
+      const product = products.find(p => p.id === id);
+      cb(product);
+    })
   }
 };
