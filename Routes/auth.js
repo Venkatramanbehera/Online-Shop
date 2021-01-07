488f76e4a0fec52649d15a2ba9a2da42355e1118
Venkatramanbehera <Venkatramanbehera1@gmail.com>
2021-01-07 10:52:56 +0530

    adding some authentication and auth middleware

--- Routes/auth.js
+++ Routes/auth.js
@@ -5,7 +5,13 @@ const router = express.Router();
 const authController = require('../controllers/auth');
 
 router.get('/login',authController.getLogin);
+
 router.post('/login',authController.postLogin);
+
+router.get('/signup',authController.getSignup);
+
+router.post('/signup',authController.postSignup);
+
 router.post('/logout',authController.postLogout);
 
 module.exports = router;
