2ac065f3da8e7c4000f80ada1a4d49c9313c495b
Venkatramanbehera <Venkatramanbehera1@gmail.com>
2021-01-17 15:07:09 +0530

    adding pagination and async requst

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,19 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 
-
+const ITEMS_PER_PAGE = 1;
 const pdfDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/orders');
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-  .then( products => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments().then( numProduct => {
+    totalItems = numProduct;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  }).then( products => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+      pageTitle: 'Shop',
+      path: '/products',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
   }).catch( err => {
     const error = new Error(err);
@@ -40,13 +53,25 @@ exports.getProduct = (req, res, next) =>{
 }
 
 exports.getIndex = (req,res,next)=>{
-  Product.find()
-  .then( products => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments().then( numProduct => {
+    totalItems = numProduct;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  }).then( products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
-      
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
   })
   .catch( err => {
